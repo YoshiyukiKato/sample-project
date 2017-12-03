2d7f828e9bc823e47744b60ef5bd1cadecd64082
YoshiyukiKato <yo.shuuuuki@gmail.com>
2017-12-04 02:05:55 +0900

    update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,8 +3,8 @@
  */
 public class App {
     public String getGreeting() {
-        return "Hello world! for jenkins:)";
-    }
+        return "Hello world! in master ;)";
+    
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
